de750bb6779cfb7e0a127607976fbc7178a5705d
Tony K. <tony-koke@outlook.com>
2024-02-12 11:15:01 +0000

    Develop Backend Update

--- server/routes/goalsRoutes.js
+++ server/routes/goalsRoutes.js
@@ -2,7 +2,26 @@ const express = require("express");
 const router = express.Router();
 const { signUp, signIn } = require("../controllers/goalsControllers");
 
+// Authentication
 router.post("/signup", signUp);
 router.post("/signin", signIn);
+
+// Main Goals
+router.post("/mainGoal",);
+router.get("/mainGoal",);
+router.put("/mainGoal",);
+router.delete("/mainGoal",);
+
+// Sub Goals
+router.post("/subGoal",);
+router.get("/subGoal",);
+router.put("/subGoal",);
+router.delete("/subGoal",);
+
+
+// Header Data - Header title, quote and background
+router.post("/headerData",);
+router.get("/headerData",);
+router.put("/headerData",);
 
 module.exports = router;
