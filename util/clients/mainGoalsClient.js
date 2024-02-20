16e02567034429bcd035e3f212a79d5026f1812e
Tony <97091194+TKPending@users.noreply.github.com>
2024-02-20 11:56:48 +0000

    Updated endpoints

--- util/clients/mainGoalsClient.js
+++ util/clients/mainGoalsClient.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const SERVER = "http://localhost:3001";
+const SERVER = "https://dozebackend.onrender.com" // "http://localhost:3001";
 
 class MainGoalsClient {
   //POST
