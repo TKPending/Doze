94ed8282df6ddf8eb3e7d1e3dd28408ed2c49989
Tony K. <tony-koke@outlook.com>
2024-02-22 22:51:18 +0000

    D-29 Testing for Username and Password (Backend)

--- __tests__/frontend/SubGoal.test.jsx
+++ __tests__/frontend/SubGoal.test.jsx
@@ -3,128 +3,126 @@ import { render, screen, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import SubGoal from "../../components/SubGoalComponent/SubGoal";
 
-
 //Mock Axios module
-jest.mock('axios');
-
+jest.mock("axios");
 
 describe(SubGoal, () => {
-    it("renders SubGoal component without crashing", () => {
-        render(<SubGoal />);
-    });
+  it("renders SubGoal component without crashing", () => {
+    render(<SubGoal />);
+  });
 
+  it("updates value when something is typed into the input field", () => {
+    render(<SubGoal />);
+    const titleInput = screen.getByPlaceholderText("Title");
+    titleInput.value = "Learn French";
+    expect(titleInput.value).toBe("Learn French");
+  });
 
-//     it("updates value when something is typed into the input field", () => {
-//         render(<SubGoal />);
-//         const titleInput = screen.getByPlaceholderText("Title");
-//         titleInput.value = "Learn French";
-//         expect(titleInput.value).toBe("Learn French");
-//     })
+  it("updates value when an option is selected on the status dropdown", () => {
+    render(<SubGoal />);
+    const statusSelect = screen.getByTestId("status-select");
+    fireEvent.change(statusSelect, { target: { value: "Complete" } });
+    expect(statusSelect.value).toBe("Complete");
+  });
 
-//     it("updates value when an option is selected on the status dropdown", () => {
-//         render(<SubGoal />);
-//         const statusSelect = screen.getByTestId("status-select");
-//         fireEvent.change(statusSelect, { target: { value: "Complete" } });
-//         expect(statusSelect.value).toBe("Complete");
-//     })
+  it("Updates value when 'a' is typed into the textarea field", () => {
+    render(<SubGoal />);
+    const descriptionInput = screen.getByPlaceholderText(
+      "Additional information"
+    );
+    descriptionInput.value = "a";
+    expect(descriptionInput.value).toBe("a");
+  });
 
-//     it("Updates value when 'a' is typed into the textarea field", () => {
-//         render(<SubGoal />);
-//         const descriptionInput = screen.getByPlaceholderText("Additional information");
-//         descriptionInput.value = "a";
-//         expect(descriptionInput.value).toBe("a");
-//     })
+  it("Renders the tags in the component after clicking the button for handleAddTag", async () => {
+    render(<SubGoal />);
+    const tagsInput = screen.getByPlaceholderText("Tags");
 
+    fireEvent.change(tagsInput, { target: { value: "Travel" } });
 
-//     it("Renders the tags in the component after clicking the button for handleAddTag", async () => {
-//         render(<SubGoal />);
-//         const tagsInput = screen.getByPlaceholderText("Tags");
+    const colourRadioButton = screen.getByDisplayValue("blue");
+    fireEvent.click(colourRadioButton);
 
-//         fireEvent.change(tagsInput, { target: {value: "Travel"}});
+    const addTagButton = screen.getByText("+");
+    fireEvent.click(addTagButton);
 
-//         const colourRadioButton = screen.getByDisplayValue("blue");
-//         fireEvent.click((colourRadioButton));
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    const renderedTag = screen.getByText("Travel");
+    expect(renderedTag).toBeInTheDocument();
+  });
 
-//         const addTagButton = screen.getByText("+");
-//         fireEvent.click(addTagButton);
+  it("alerts when trying to add a tag with nothing in the input field but a colour radio button is checked", () => {
+    const mockAlert = jest.spyOn(window, "alert").mockImplementation(() => {});
+    render(<SubGoal />);
+    const tagsInput = screen.getByPlaceholderText("Tags");
+    fireEvent.change(tagsInput, { target: { value: "" } });
 
-//        await new Promise((resolve) => setTimeout(resolve, 1000));
-//        const renderedTag = screen.getByText("Travel");
-//        expect(renderedTag).toBeInTheDocument();
-//     });
+    const colourRadioButton = screen.getByDisplayValue("pink");
+    fireEvent.click(colourRadioButton, { target: { value: "pink" } });
 
+    const addTagButton = screen.getByText("+");
+    fireEvent.click(addTagButton);
 
-//     it("alerts when trying to add a tag with nothing in the input field but a colour radio button is checked", () => {
-//         const mockAlert = jest.spyOn(window, "alert").mockImplementation(() => {});
-//         render(<SubGoal />);
-//         const tagsInput = screen.getByPlaceholderText("Tags");
-//         fireEvent.change(tagsInput, { target: {value: ""}});
+    expect(mockAlert).toHaveBeenCalledWith(
+      "Please enter a tag and select a colour"
+    );
+  });
 
-//         const colourRadioButton = screen.getByDisplayValue("pink");
-//         fireEvent.click(colourRadioButton, { target: {value: "pink"}});
+  it("alerts when trying to add a tag with no colour radio button selected but with text in the input field", () => {
+    const mockAlert = jest.spyOn(window, "alert").mockImplementation(() => {});
+    render(<SubGoal />);
+    const tagsInput = screen.getByPlaceholderText("Tags");
+    fireEvent.change(tagsInput, { target: { value: "Travel" } });
 
-//         const addTagButton = screen.getByText("+");
-//         fireEvent.click(addTagButton);
+    const addTagButton = screen.getByText("+");
+    fireEvent.click(addTagButton);
 
-//         expect(mockAlert).toHaveBeenCalledWith("Please enter a tag and select a colour");
-//     })
+    expect(mockAlert).toHaveBeenCalledWith(
+      "Please enter a tag and select a colour"
+    );
+  });
 
-//     it("alerts when trying to add a tag with no colour radio button selected but with text in the input field", () => {
-//         const mockAlert = jest.spyOn(window, "alert").mockImplementation(() => {});
-//         render(<SubGoal />);
-//         const tagsInput = screen.getByPlaceholderText("Tags");
-//         fireEvent.change(tagsInput, { target: {value: "Travel"}});
+  it("Deletes the tag from the screen when the 'x' button on the tags is clicked", async () => {
+    render(<SubGoal />);
+    const tagsInput = screen.getByPlaceholderText("Tags");
 
-//         const addTagButton = screen.getByText("+");
-//         fireEvent.click(addTagButton);
+    fireEvent.change(tagsInput, { target: { value: "Travel" } });
 
-//         expect(mockAlert).toHaveBeenCalledWith("Please enter a tag and select a colour")
-//     })
+    const colourRadioButton = screen.getByDisplayValue("blue");
+    fireEvent.click(colourRadioButton);
 
-  
+    const addTagButton = screen.getByText("+");
+    fireEvent.click(addTagButton);
 
-//     it("Deletes the tag from the screen when the 'x' button on the tags is clicked", async () => {
-//         render(<SubGoal />);
-//         const tagsInput = screen.getByPlaceholderText("Tags");
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    const renderedTag = screen.queryByText("Travel");
 
-//         fireEvent.change(tagsInput, { target: {value: "Travel"}});
+    const deleteButton = screen.getByTestId("remove-tag");
+    fireEvent.click(deleteButton);
 
-//         const colourRadioButton = screen.getByDisplayValue("blue");
-//         fireEvent.click((colourRadioButton));
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    expect(renderedTag).not.toBeInTheDocument();
+  });
 
-//         const addTagButton = screen.getByText("+");
-//         fireEvent.click(addTagButton);
+  it("Shows an alert when the 'Save' button is clicked when nothing in title, icon or status.", () => {
+    render(<SubGoal />);
+    const mockAlert = jest.spyOn(window, "alert").mockImplementation(() => {});
 
-//        await new Promise((resolve) => setTimeout(resolve, 1000));
-//        const renderedTag = screen.queryByText("Travel");
-      
-//        const deleteButton = screen.getByTestId("remove-tag");
-//        fireEvent.click(deleteButton);
+    const saveButton = screen.getByText("Save");
+    fireEvent.click(saveButton);
 
-//        await new Promise((resolve) => setTimeout(resolve, 1000));
-//        expect(renderedTag).not.toBeInTheDocument();
-//     });
+    expect(mockAlert).toHaveBeenCalledWith(
+      "Please fill in title, status and add an icon"
+    );
+  });
 
-//     it("Shows an alert when the 'Save' button is clicked when nothing in title, icon or status.", () => {
-//         render(<SubGoal/>);
-//         const mockAlert = jest.spyOn(window, "alert").mockImplementation(() => {});
-        
-//         const saveButton = screen.getByText("Save");
-//         fireEvent.click(saveButton);
+  //   it("Opens emoji picker when emoji is clicked", async () => {
+  //     render(<SubGoal/>);
+  //     const emojiSelect = screen.getByTestId("emoji-icon");
+  //     fireEvent.click(emojiSelect);
+  //     const emojiPicker = (<Picker/>);
 
-//         expect(mockAlert).toHaveBeenCalledWith("Please fill in title, status and add an icon");
+  //     expect(emojiPicker).toBeInTheDocument
 
-//     })
-
-//     //   it("Opens emoji picker when emoji is clicked", async () => {
-//     //     render(<SubGoal/>);
-//     //     const emojiSelect = screen.getByTestId("emoji-icon");
-//     //     fireEvent.click(emojiSelect);
-//     //     const emojiPicker = (<Picker/>);
-
-//     //     expect(emojiPicker).toBeInTheDocument
-
-//     //     })
-
-
-})
+  //     })
+});
