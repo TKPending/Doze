1e458fc14dddb01290b89617019dfb7a17824816
Tony K. <tony-koke@outlook.com>
2024-02-29 00:24:35 +0000

    D-48 Error Handling

--- components/MainGoalComponents/MainGoal.jsx
+++ components/MainGoalComponents/MainGoal.jsx
@@ -227,8 +227,8 @@ const MainGoal = ({
             </div>
           )}
 
-        {emptyAllSubGoals && <SuccessMessage message={SUCCESS_MESSAGES.REMOVE_ALL_SUBGOALS} />}
-      <div className="md:w-1/2 h-full relative">
+          {emptyAllSubGoals && <SuccessMessage message={SUCCESS_MESSAGES.REMOVE_ALL_SUBGOALS} />}
+          <div className="md:w-1/2 h-full relative">
       
         <form id="goalForm" onSubmit={submitHandler}>
           <input
@@ -260,179 +260,161 @@ const MainGoal = ({
              </div>
             </div>
           )}
+          
+            <div className="">
+              <button onClick={toggleEmojiPicker} className="mb-5 mt-6">
 
-          <div className="md:w-1/2 h-full relative">
-            <form id="goalForm" onSubmit={submitHandler}>
+                <span className="text-6xl">{mainGoalData.icon}</span>
+              </button>
+              {isOpen && (
+                <div className="absolute sm:left-5">
+                  <Picker
+                    data={data}
+                    onEmojiSelect={handleEmoji}
+                    onClickOutside={toggleEmojiPicker}
+                  />
+                </div>
+              )}
+            </div>
+           
+            <p className="text-center mb-2.5 text-gray-800">Select a start date</p>
+            
               <input
-                data-testid="title"
-                name="title"
-                type="text"
-                className="input text-3xl mb-5"
-                value={mainGoalData.title}
+                data-testid="startDate"
+                name="startDate"
+                type="date"
+                className="input border-indigo-600 focus:border-indigo-600 focus:outline-indigo-600 mb-8 p-2 w-56 h-8"
                 onChange={handleInputValue}
+                value={mainGoalData.startDate}
               />
-              <div className="relative flex items-center justify-center border border-indigo-600 rounded-md flex-col w-full h-full">
-                <button onClick={toggleEmojiPicker} className="mb-5 mt-6">
-                  <span className="text-6xl">{mainGoalData.icon}</span>
-                </button>
-                {isOpen && (
-                  <div className="absolute">
-                    <Picker
-                      data={data}
-                      onEmojiSelect={handleEmoji}
-                      onClickOutside={toggleEmojiPicker}
-                    />
-                  </div>
-                )}
+          
 
-                <p className="text-center mb-2.5 text-gray-800">
-                  Select a start date
-                </p>
+            {/* Button for changing Status */}
+            
+              <select
+                className="outline-indigo-600 border-indigo-600 border rounded-md focus:border-indigo-600 p-2 mb-8 w-56"
+                name="status"
+                onChange={handleInputValue}
+                value={mainGoalData.status}
+              >
+                <option value="">Select a status</option>
+                <option value="to-do">To-do</option>
+                <option value="in-progress">In progress</option>
+                <option value="complete">Complete</option>
+              </select>
 
+            {/* Tag input and colour changer */}
+            <div className="flex justify-center items-center mb-2.5">
+              
+              <input
+                name="tag"
+                type="text"
+                value={tagInput}
+                onChange={handleTagInputChange}
+                className="input border border-indigo-600 focus:border-indigo-600 focus:outline-indigo-600 w-44 h-8"
+                placeholder="Tags"
+                disabled={errorMessage}
+              />
+              <button
+                className="flex items-center border bg-indigo-600 text-white rounded-md hover:bg-white hover:border-indigo-600 hover:text-black transition duration-200 p-2.5 ml-4 h-8 w-8"
+                onClick={handleAddTag}
+              >
+                +
+              </button>
+              </div>
+
+              {/* Buttons for changing tag colour */}
+              <div className="flex justify-center items-center">
                 <input
-                  data-testid="startDate"
-                  name="startDate"
-                  type="date"
-                  className="input border-indigo-600 focus:border-indigo-600 focus:outline-indigo-600 mb-8 p-2 w-56 h-8"
-                  onChange={handleInputValue}
-                  value={mainGoalData.startDate}
+                  type="radio"
+                  id="blue"
+                  name="colour"
+                  value="blue"
+                  disabled={errorMessage}
+                  defaultChecked={selectedColour === "blue"}
+                  onChange={() => handleColourChange("blue")}
+                  className="radio border-blue-400 checked:bg-blue-400 bg-blue-400 mt-1"
                 />
 
-                {/* Button for changing Status */}
+                <input
+                  type="radio"
+                  id="red"
+                  name="colour"
+                  value="red"
+                  checked={selectedColour === "red"}
+                  onChange={() => handleColourChange("red")}
+                  className="radio border-red-400 checked:bg-red-400 bg-red-400 ml-2 mt-1"
+                />
 
-                <select
-                  className="outline-indigo-600 border-indigo-600 border rounded-md focus:border-indigo-600 p-2 mb-8 w-56"
-                  name="status"
-                  onChange={handleInputValue}
-                  value={mainGoalData.status}
-                >
-                  <option value="">Select a status</option>
-                  <option value="to-do">To-do</option>
-                  <option value="in-progress">In progress</option>
-                  <option value="complete">Complete</option>
-                </select>
+                <input
+                  type="radio"
+                  id="lime"
+                  name="colour"
+                  value="lime"
+                  disabled={errorMessage}
+                  checked={selectedColour === "lime"}
+                  onChange={() => handleColourChange("lime")}
+                  className="radio border-lime-400 checked:bg-lime-400 bg-lime-400 ml-2 mt-1"
+                />
 
-                {/* Tag input and colour changer */}
-                <div className="flex justify-center items-center mb-2.5">
-                  <input
-                    name="tag"
-                    type="text"
-                    value={tagInput}
-                    onChange={handleTagInputChange}
-                    className="input border border-indigo-600 focus:border-indigo-600 focus:outline-indigo-600 w-44 h-8"
-                    placeholder="Tags"
-                  />
-                  <button
-                    className="flex items-center border bg-indigo-600 text-white rounded-md hover:bg-white hover:border-indigo-600 hover:text-black transition duration-200 p-2.5 ml-4 h-8 w-8"
-                    onClick={handleAddTag}
-                  >
-                    +
-                  </button>
-                </div>
+                <input
+                  type="radio"
+                  id="purple"
+                  name="colour"
+                  value="purple"
+                  disabled={errorMessage}
+                  checked={selectedColour === "purple"}
+                  onChange={() => handleColourChange("purple")}
+                  className="radio border-purple-400 checked:bg-purple-400 bg-purple-400 ml-2 mt-1"
+                />
 
-                {/* Buttons for changing tag colour */}
-                <div className="flex justify-center items-center">
-                  <input
-                    type="radio"
-                    id="blue"
-                    name="colour"
-                    value="blue"
-                    defaultChecked={selectedColour === "blue"}
-                    disabled={errorMessage}
-                    onChange={() => handleColourChange("blue")}
-                    className="radio border-blue-400 checked:bg-blue-400 bg-blue-400 mt-1"
-                  />
+                <input
+                  type="radio"
+                  id="yellow"
+                  name="colour"
+                  value="yellow"
+                  disabled={errorMessage}
+                  checked={selectedColour === "yellow"}
+                  onChange={() => handleColourChange("yellow")}
+                  className="radio border-yellow-400 checked:bg-yellow-400 bg-yellow-400 ml-2 mt-1"
+                />
 
-                  <input
-                    type="radio"
-                    id="red"
-                    name="colour"
-                    value="red"
-                    checked={selectedColour === "red"}
-                    disabled={errorMessage}
-                    onChange={() => handleColourChange("red")}
-                    className="radio border-red-400 checked:bg-red-400 bg-red-400 ml-2 mt-1"
-                  />
+                <input
+                  type="radio"
+                  id="orange"
+                  name="colour"
+                  value="orange"
+                  disabled={errorMessage}
+                  checked={selectedColour === "orange"}
+                  onChange={() => handleColourChange("orange")}
+                  className="radio border-orange-400 checked:bg-orange-400 bg-orange-400 ml-2 mt-1"
+                />
 
-                  <input
-                    type="radio"
-                    id="lime"
-                    name="colour"
-                    value="lime"
-                    checked={selectedColour === "lime"}
-                    disabled={errorMessage}
-                    onChange={() => handleColourChange("lime")}
-                    className="radio border-lime-400 checked:bg-lime-400 bg-lime-400 ml-2 mt-1"
-                  />
+                <input
+                  type="radio"
+                  id="pink"
+                  name="colour"
+                  value="pink"
+                  disabled={errorMessage}
+                  checked={selectedColour === "pink"}
+                  onChange={() => handleColourChange("pink")}
+                  className="radio border-pink-400 checked:bg-pink-400 bg-pink-400 ml-2 mt-1"
+                />
+              </div>
 
-                  <input
-                    type="radio"
-                    id="purple"
-                    name="colour"
-                    value="purple"
-                    checked={selectedColour === "purple"}
-                    disabled={errorMessage}
-                    onChange={() => handleColourChange("purple")}
-                    className="radio border-purple-400 checked:bg-purple-400 bg-purple-400 ml-2 mt-1"
-                  />
-
-                  <input
-                    type="radio"
-                    id="yellow"
-                    name="colour"
-                    value="yellow"
-                    checked={selectedColour === "yellow"}
-                    disabled={errorMessage}
-                    onChange={() => handleColourChange("yellow")}
-                    className="radio border-yellow-400 checked:bg-yellow-400 bg-yellow-400 ml-2 mt-1"
-                  />
-
-                  <input
-                    type="radio"
-                    id="orange"
-                    name="colour"
-                    value="orange"
-                    disabled={errorMessage}
-                    checked={selectedColour === "orange"}
-                    onChange={() => handleColourChange("orange")}
-                    className="radio border-orange-400 checked:bg-orange-400 bg-orange-400 ml-2 mt-1"
-                  />
-
-                  <input
-                    type="radio"
-                    id="pink"
-                    name="colour"
-                    value="pink"
-                    checked={selectedColour === "pink"}
-                    disabled={errorMessage}
-                    onChange={() => handleColourChange("pink")}
-                    className="radio border-pink-400 checked:bg-pink-400 bg-pink-400 ml-2 mt-1"
-                  />
-                </div>
-
-
-                <div
-                  className={`mt-2.5 mb-2.5 ${
-                    mainGoalData?.tags?.length > 3
-                      ? "grid grid-cols-4 gap-1"
-                      : "flex"
-                  }`}
-                >
-                  {mainGoalData?.tags?.map((tag, index) => (
-                    <span data-testid="tag" key={index}>
-                      <div
-                        className={`badge bg-${tag.colour}-400 gap-2 p-4 text-white`}
-                      >
-                        {tag.text}
-                        <a
-                          onClick={() => handleRemoveTag(index)}
-                          className="inline-block cursor-pointer w-2.5 opacity-0 hover:opacity-100 hover:text-white "
-                        >
-                          x
-                        </a>
-                      </div>
-                    </span>
-
+              <div className={`mt-2.5 mb-2.5 ${mainGoalData.tags.length > 3 ? 'grid grid-cols-4 gap-1' : 'flex'}`}>
+                {mainGoalData.tags.map((tag, index) => (
+                  <span data-testid="tag"  key={index}>
+                    <div className={`badge bg-${tag.colour}-400 gap-2 p-4 text-white`}>
+                      {tag.text}
+                      <a onClick={() => handleRemoveTag(index)} className="inline-block cursor-pointer w-2.5 opacity-0 hover:opacity-100 hover:text-white ">
+                        x
+                      </a>
+                    </div>
+                  </span>
+                ))}
+              </div>
+            
 
             
             {/* Description input */}
@@ -443,7 +425,7 @@ const MainGoal = ({
                 value={mainGoalData.description}
                 className="textarea h-18 input-bordered md:w-1/2 w-60 mb-5 border-indigo-600 focus:border-indigo-600 focus:outline-indigo-600"
               ></textarea>
-                  
+           
 <span className="h-0.5 bg-indigo-600 opacity-25 mt-10 w-5/6"></span>
             {/* Sub Goals */}
             {mainGoalData._id && (
@@ -462,56 +444,55 @@ const MainGoal = ({
                       onClick={handleOnClick}
                     />
                   ))}
-                </div>
 
-                      {mainGoalData.subGoals.length === 0 && (
-                        <div>
-                          <p className="font-bold text-center text-neutral-400">
-                            Add goals which will help with achieving your mains
-                            goals!
-                          </p>
-                        </div>
-                      )}
-
-                      <AddSubGoal setIsModalVisible={setSubGoalModalVisible} />
+                  {mainGoalData.subGoals.length === 0 && (
+                    <div>
+                      <p className="font-bold text-center text-neutral-400">
+                        Add goals which will help with achieving your mains
+                        goals!
+                      </p>
                     </div>
-                  </div>
-                )}
-
-                <div className="flex justify-between">
-                  {mainGoalData._id && (
-                    <button
-                      className="absolute left-4 bottom-4 text-indigo-600 hover:text-white hover:bg-indigo-600 border border-indigo-600 rounded-md p-2 pl-5 pr-5"
-                      data-testid="delete"
-                      onClick={onDeleteMainGoal}
-                    >
-                      Delete
-                    </button>
                   )}
-                  <button className="absolute right-4 bottom-4 text-indigo-600 hover:text-white hover:bg-indigo-600 border border-indigo-600 rounded-md p-2 pl-5 pr-5">
-                    Save
-                  </button>
+
+                  <AddSubGoal setIsModalVisible={setSubGoalModalVisible} />
                 </div>
               </div>
-            </form>
-
-            {isSubGoalModalVisible && (
-              <SubGoal
-                setIsModalVisible={setSubGoalModalVisible}
-                setTaskUpdated={setTaskUpdated}
-                goalTitle={mainGoalData.title}
-              />
             )}
 
-            {isEditModalVisible && (
-              <EditSubGoal
-                setIsEditModalVisible={setIsEditModalVisible}
-                taskClicked={taskClicked}
-                setTaskUpdated={setTaskUpdated}
-              />
-            )}
+            <div className="flex justify-between">
+              {mainGoalData._id && (
+                <button
+                  className="absolute left-4 bottom-4 text-indigo-600 hover:text-white hover:bg-indigo-600 border border-indigo-600 rounded-md p-2 pl-5 pr-5"
+                  data-testid="delete"
+                  onClick={onDeleteMainGoal}
+                >
+                  Delete
+                </button>
+              )}
+              <button className="absolute right-4 bottom-4 text-indigo-600 hover:text-white hover:bg-indigo-600 border border-indigo-600 rounded-md p-2 pl-5 pr-5">
+                Save
+              </button>
+            </div>
           </div>
-        </>
+        </form>
+
+        {isSubGoalModalVisible && (
+          <SubGoal
+            setIsModalVisible={setSubGoalModalVisible}
+            setTaskUpdated={setTaskUpdated}
+            goalTitle={mainGoalData.title}
+          />
+        )}
+
+        {isEditModalVisible && (
+          <EditSubGoal
+            setIsEditModalVisible={setIsEditModalVisible}
+            taskClicked={taskClicked}
+            setTaskUpdated={setTaskUpdated}
+          />
+        )}
+      </div>
+      </>
       )}
     </div>
   );
